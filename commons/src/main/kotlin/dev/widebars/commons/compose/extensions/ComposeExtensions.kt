e68dab2bee4c71ee5504e0bfffdfa7243de7ddd8
WideBars <open.merge@outlook.com>
2025-12-23 17:37:17 +0300

    lastError and ComposeExtensions

--- commons/src/main/kotlin/dev/widebars/commons/compose/extensions/ComposeExtensions.kt
+++ commons/src/main/kotlin/dev/widebars/commons/compose/extensions/ComposeExtensions.kt
@@ -27,8 +27,11 @@ import androidx.lifecycle.compose.LifecycleStartEffect
 import dev.widebars.commons.compose.system_ui_controller.rememberSystemUiController
 import dev.widebars.commons.compose.theme.SimpleTheme
 import dev.widebars.commons.compose.theme.isLitWell
+import dev.widebars.commons.extensions.baseConfig
 import dev.widebars.commons.extensions.darkenColor
+import dev.widebars.commons.extensions.getContrastColor
 import dev.widebars.commons.extensions.googlePlayDevUrlRes
+import dev.widebars.commons.extensions.isDynamicTheme
 import dev.widebars.commons.extensions.launchViewIntent
 
 fun Context.getActivity(): Activity {
@@ -151,24 +154,28 @@ fun ComponentActivity.enableEdgeToEdgeSimple() {
 }
 
 @Composable
-internal fun TransparentSystemBars(darkIcons: Boolean = !isSystemInDarkTheme()) {
+internal fun TransparentSystemBars() {
     val systemUiController = rememberSystemUiController()
 
-    DisposableEffect(systemUiController, darkIcons) {
-        systemUiController.setSystemBarsColor(
-            color = Color.Transparent,
-            darkIcons = darkIcons,
-            isNavigationBarContrastEnforced = true
-        )
-        onDispose { }
-    }
-//    SideEffect {
+//    DisposableEffect(systemUiController, darkIcons) {
 //        systemUiController.setSystemBarsColor(
 //            color = Color.Transparent,
 //            darkIcons = darkIcons,
 //            isNavigationBarContrastEnforced = true
 //        )
+//        onDispose { }
 //    }
+
+    val localContext = LocalContext.current
+    val darkIcons = (localContext.isDynamicTheme() && !isSystemInDarkTheme()) ||
+        (localContext.baseConfig.backgroundColor.getContrastColor() != android.graphics.Color.WHITE)
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent,
+            darkIcons = darkIcons,
+            isNavigationBarContrastEnforced = true
+        )
+    }
 }
 
 @Composable
